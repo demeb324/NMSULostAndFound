13a938367386c392caeb1d3b1608b6b23fb9e5ff
Esteban Saenz <estebansaenz356@gmail.com>
2024-11-12 11:53:57 -0700

    Added menu functionality to admin page

--- src/com/Aggie_FindIt/AdminPageController.java
+++ src/com/Aggie_FindIt/AdminPageController.java
@@ -20,7 +20,7 @@ public class AdminPageController implements Initializable{
     @FXML
     private TextField itemDescription, itemColor; 
     @FXML 
-    private Button completeReturn, cancelReturn;
+    private Button completeReturn, cancel, addItem;
     @FXML
     private TextArea returnText;
     private String[] categories = {"Phone", "Tablet", "Computer", "School supply", "Personal Item"};
@@ -32,31 +32,92 @@ public class AdminPageController implements Initializable{
     }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        addItem.setOpacity(0);
+        itemDescription.setOpacity(0);
+        itemColor.setOpacity(0);
+        itemCategory.setOpacity(0);
+        itemCategoryLabel.setOpacity(0);
+        itemDate.setOpacity(0);
+        itemDateLabel.setOpacity(0);
+        completeReturn.setOpacity(0);
+        cancel.setOpacity(0);
+        returnText.setOpacity(0);
         itemCategory.getItems().addAll(categories);
     }
     @FXML
     private void addItem() {
         itemDescription.setDisable(false);
+        itemDescription.setOpacity(100);
         itemColor.setDisable(false);
+        itemColor.setOpacity(100);
         itemCategory.setDisable(false);
+        itemCategory.setOpacity(100);
         itemCategoryLabel.setDisable(false);
+        itemCategoryLabel.setOpacity(100);
         itemDate.setDisable(false);
+        itemDate.setOpacity(100);
         itemDateLabel.setDisable(false);
+        itemDateLabel.setOpacity(100);
+        addItem.setDisable(false);
+        addItem.setOpacity(100);
         completeReturn.setDisable(true);
-        cancelReturn.setDisable(true);
+        completeReturn.setOpacity(0);
+        cancel.setDisable(false);
+        cancel.setOpacity(100);
         returnText.setDisable(true);
+        returnText.setOpacity(0);
     }
     @FXML
     private void itemReturn() {
         itemDescription.setDisable(true);
+        itemDescription.setOpacity(0);
         itemColor.setDisable(true);
+        itemColor.setOpacity(0);
         itemCategory.setDisable(true);
+        itemCategory.setOpacity(0);
         itemCategoryLabel.setDisable(true);
+        itemCategoryLabel.setOpacity(0);
         itemDate.setDisable(true);
+        itemDate.setOpacity(0);
         itemDateLabel.setDisable(true);
+        itemDateLabel.setOpacity(0);
+        addItem.setDisable(true);
+        addItem.setOpacity(0);
         completeReturn.setDisable(false);
-        cancelReturn.setDisable(false);
+        completeReturn.setOpacity(100);
+        cancel.setDisable(false);
+        cancel.setOpacity(100);
         returnText.setDisable(false);
+        returnText.setOpacity(100);
+    }
+
+    @FXML
+    private void cancel() {
+        itemDescription.setDisable(true);
+        itemDescription.setOpacity(0);
+        itemColor.setDisable(true);
+        itemColor.setOpacity(0);
+        itemCategory.setDisable(true);
+        itemCategory.setOpacity(0);
+        itemCategoryLabel.setDisable(true);
+        itemCategoryLabel.setOpacity(0);
+        itemDate.setDisable(true);
+        itemDate.setOpacity(0);
+        itemDateLabel.setDisable(true);
+        itemDateLabel.setOpacity(0);
+        addItem.setDisable(true);
+        addItem.setOpacity(0);
+        completeReturn.setDisable(true);
+        completeReturn.setOpacity(0);
+        cancel.setDisable(true);
+        cancel.setOpacity(0);
+        returnText.setDisable(true);
+        returnText.setOpacity(0);
+    }
+
+    @FXML
+    private void submitItem(){
+
     }
 
 }
